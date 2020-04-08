ffb408f80c14ccb5ab3bb132a46be3b6b2fe882f
sfongue <sfongue@gmail.com>
2020-04-08 11:27:18 +0200

    add user model and connection to db

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 8000;
+const app = express();
 
 app.use(bodyParser.json());
 
